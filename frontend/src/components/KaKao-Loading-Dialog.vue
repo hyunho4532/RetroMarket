aea966ce2e80ed396d7947aca52fc978f8bee54d
Lee Hyun Ho <118269278+hynuho4562@users.noreply.github.com>
2023-10-14 00:07:37 +0900

    2023-10-13 문의 하기 기능 구현

---
+++ frontend/src/components/KaKao-Loading-Dialog.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="modal" tabindex="-1" v-if="showKaKaoModalDialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header" ref="productModalInsert">
+          <h5 class="modal-title">상담 센터 조회 중!</h5>
+        </div>
+        <div class="modal-body">
+          <div>
+            <iframe src="https://pf.kakao.com/_xcaSjG/chat" frameborder="0" style="width: 100%; height: 100vh;"></iframe>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="closeModal">닫기</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  display: block;
+  justify-content: center;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  overflow: auto;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0,0.4);
+  padding-top: 60px;
+  padding-right: 400px;
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 900px;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      showKaKaoModalDialog: false,
+    }
+  },
+
+  methods: {
+    openModal() {
+      this.showKaKaoModalDialog = true;
+    },
+    closeModal() {
+      this.showKaKaoModalDialog = false;
+    },
+  }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,10 @@
   <div>
     <SwiperBody></SwiperBody>
 
+    <div class="contact-button-container" ref="contactButtonContainer">
+      <button type="button" class="btn btn-primary" id="product-insert-button" @click="openKaKaoModal">문의 하기</button>
+    </div>
+
     <div class="recent-items" style="display: flex;" ref="recentItems">
       <h5>최근에 판매되고 있는 물품들</h5>
       <input type="text" class="form-control" v-model="searchAddress" style="width: 240px; height: 30px; margin-left: 12px;" placeholder="주소 검색">
@@ -27,6 +31,10 @@
     <ModalDialog ref="modalDialog">
       <button @click="closeModal">닫기</button>
     </ModalDialog>
+
+    <KaKaoLoadingDialog ref="showKaKaoModalDialog">
+      <button @click="closeKaKaoModal">닫기</button>
+    </KaKaoLoadingDialog>
   </div>
 </template>
 
@@ -38,6 +46,7 @@ import ProgressCardBody from "@/components/card/status/progress/Progress-Card-Bo
 import ModalDialog from "@/components/Modal-Dialog.vue";
 import axios from "axios";
 import gsap from 'gsap';
+import KaKaoLoadingDialog from "@/components/KaKao-Loading-Dialog.vue";
 
 export default {
 
@@ -51,6 +60,7 @@ export default {
   },
 
   components: {
+    KaKaoLoadingDialog,
     ModalDialog,
     SwiperBody,
     ProcessCardBody,
@@ -64,6 +74,14 @@ export default {
     },
     closeModal() {
       this.$refs.modalDialog.closeModal();
+    },
+
+    openKaKaoModal() {
+      this.$refs.showKaKaoModalDialog.openModal();
+    },
+
+    closeKaKaoModal() {
+      this.$refs.showKaKaoModalDialog.closeModal();
     },
 
     searchProducts() {
@@ -84,6 +102,10 @@ export default {
     openChatModal(roomId) {
       this.$refs.chatDialog.openChatModal(roomId);
     },
+
+    openKaKaoTalkChat() {
+      window.location.href = 'https://pf.kakao.com/_xcaSjG/chat'
+    }
   },
 
   mounted() {
@@ -94,6 +116,17 @@ export default {
     gsap.from(recentItems, { opacity: 0, x: 100, duration: 2 })
     gsap.fromTo(recentFinishItems, { x: 100, opacity: 0 }, { x: 0, duration: 1, opacity: 1, delay: 2 })
     gsap.fromTo(recentProgressItems, { x: 100, opacity: 0 }, { x: 0, duration: 1, opacity: 1, delay: 4 })
+
+    const contactButtonContainer = this.$refs.contactButtonContainer;
+    const offsetTop = contactButtonContainer.offsetTop;
+
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > offsetTop) {
+        contactButtonContainer.classList.add('fixed');
+      } else {
+        contactButtonContainer.classList.remove('fixed')
+      }
+    });
   }
 }
 </script>
